6780018802a85a58b9f771b01972268d4b2c59e0
tienbui <tienbui1508@gmail.com>
2023-07-23 22:36:15 +0930

    Initial Commit

---
+++ Resell/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Resell
+//
+//  Created by Tien Bui on 23/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Resell/ResellApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResellApp.swift
+//  Resell
+//
+//  Created by Tien Bui on 23/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct ResellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
